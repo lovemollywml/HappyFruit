2ea940b23c9703c4f01b08cd3391f749ae7ea71c
MollyWendy <1187820970@qq.com>
2017-06-27 13:40:15 +0800

    Loading界面加载完成

---
+++ Assets/Scripts/Home/Loading.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Loading : MonoBehaviour {
+
+    private const float count = 100f; //模拟进度条加载比例
+    public Image load_bar; //进度条
+    IEnumerator Start()
+    {
+        for (int i = 1; i <= count; i++)
+        {
+            load_bar.fillAmount += 1 / count;
+            yield return new WaitForEndOfFrame();
+        }
+        Debug.Log("Loading Over!");
+        //加载主场景
+        SceneManager.LoadScene("Home");
+    }
+}
